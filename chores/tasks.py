5006e7bc169c1376ed0ab4c660b8eed4fbab84c6
damonmickelsen <dgmickelsen@mail.wvncc.edu>
2025-09-17 20:57:47 -0600

    2.2.0.6

--- chores/tasks.py
+++ chores/tasks.py
@@ -1,6 +1,13 @@
 from celery import shared_task
+from notifications.emails.services import send_email
+
 
 @shared_task
 def run_overnight_chore_check_task():
+    send_email('emails/system/overnight_chore_task_start', "Overnight System Task: started", ['damon.mickelsen@gmail.com'])
     from django.core.management import call_command
-    call_command("run_overnight_chore_check")
+    try:
+        call_command("run_overnight_chore_check")
+        send_email('emails/system/overnight_chore_task_complete', "Overnight System Task: complete", ['damon.mickelsen@gmail.com'])
+    except Exception as e:
+        send_email('emails/system/overnight_chore_task_error', "Overnight System Task: error", ['damon.mickelsen@gmail.com'])
