aa71391fef01b075d8f9d95a6a10c15b20c5c58c
damonmickelsen <dgmickelsen@mail.wvncc.edu>
2025-09-18 00:28:36 -0600

    2.2.0.8.2

--- notifications/emails/services.py
+++ notifications/emails/services.py
@@ -39,74 +39,75 @@ def send_email(template_prefix, subject, to, context=None, from_email="the.chore
         delivered = False
 
     # Log to database
-    for recipient in to:
-        safe_context = {
-            k: (v.id if hasattr(v, "id") else v)
-            for k, v in context.items()
-        }
-        Notification.objects.create(
-            user=context.get("usertask").user,
-            notification_type="email",
-            subject=subject,
-            message=html_content,
-            template_name=template_prefix,
-            delivered=delivered,
-            meta={
-                "recipients": to,
-                "context": safe_context,
+    if 'usertask' in context.keys():
+        for recipient in to:
+            safe_context = {
+                k: (v.id if hasattr(v, "id") else v)
+                for k, v in context.items()
             }
-        )
+            Notification.objects.create(
+                user=context.get("usertask").user,
+                notification_type="email",
+                subject=subject,
+                message=html_content,
+                template_name=template_prefix,
+                delivered=delivered,
+                meta={
+                    "recipients": to,
+                    "context": safe_context,
+                }
+            )
 
 
-def send_task_assigned_email(usertask, task_url):
-    """Wrapper for 'task assigned' emails."""
-    subject = f"New Chore Assigned: {usertask.schedule_task.name}"
-    to = [usertask.user.email]
-    context = {
-        "usertask": usertask,
-        "task_url": task_url,
-    }
-    send_email("emails/chores/task_assigned", subject, to, context)
+    def send_task_assigned_email(usertask, task_url):
+        """Wrapper for 'task assigned' emails."""
+        subject = f"New Chore Assigned: {usertask.schedule_task.name}"
+        to = [usertask.user.email]
+        context = {
+            "usertask": usertask,
+            "task_url": task_url,
+        }
+        send_email("emails/chores/task_assigned", subject, to, context)
 
 
-def send_task_overdue_email(usertask, task_url):
-    """Wrapper for 'task overdue' emails."""
-    subject = f"Chore Overdue: {usertask.schedule_task.name}"
-    to = [usertask.user.email]
-    context = {
-        "usertask": usertask,
-        "task_url": task_url,
-    }
-    send_email("emails/chores/task_overdue", subject, to, context)
+    def send_task_overdue_email(usertask, task_url):
+        """Wrapper for 'task overdue' emails."""
+        subject = f"Chore Overdue: {usertask.schedule_task.name}"
+        to = [usertask.user.email]
+        context = {
+            "usertask": usertask,
+            "task_url": task_url,
+        }
+        send_email("emails/chores/task_overdue", subject, to, context)
 
 
-def send_task_incomplete_email(usertask, task_url):
-    """Wrapper for 'task overdue' emails."""
-    subject = f"Task Is Incomplete: {usertask.schedule_task.name}"
-    to = [usertask.user.email]
-    context = {
-        "usertask": usertask,
-        "task_url": task_url,
-    }
-    send_email("emails/chores/task_incomplete", subject, to, context)
+    def send_task_incomplete_email(usertask, task_url):
+        """Wrapper for 'task overdue' emails."""
+        subject = f"Task Is Incomplete: {usertask.schedule_task.name}"
+        to = [usertask.user.email]
+        context = {
+            "usertask": usertask,
+            "task_url": task_url,
+        }
+        send_email("emails/chores/task_incomplete", subject, to, context)
 
 
-def send_task_complete_email(usertask, task_url):
-    """Wrapper for 'task complete' emails."""
-    subject = f'Chore Complete: {usertask.schedule_task.name}'
-    to = [usertask.user.email]
-    context = {
-        'usertask': usertask,
-        'task_url': task_url,
-    }
-    send_email('emails/chores/task_complete', subject, to, context)
+    def send_task_complete_email(usertask, task_url):
+        """Wrapper for 'task complete' emails."""
+        subject = f'Chore Complete: {usertask.schedule_task.name}'
+        to = [usertask.user.email]
+        context = {
+            'usertask': usertask,
+            'task_url': task_url,
+        }
+        send_email('emails/chores/task_complete', subject, to, context)
 
 
-def send_task_up_next_email(usertask, task_url):
-    subject = f"Coming Up: {usertask.schedule_task.name}"
-    to = [usertask.user.email]
-    context = {
-        "usertask": usertask,
-        "task_url": task_url,
-    }
-    send_email("emails/chores/task_up_next", subject, to, context)
+    def send_task_up_next_email(usertask, task_url):
+        subject = f"Coming Up: {usertask.schedule_task.name}"
+        to = [usertask.user.email]
+        context = {
+            "usertask": usertask,
+            "task_url": task_url,
+        }
+        send_email("emails/chores/task_up_next", subject, to, context)
